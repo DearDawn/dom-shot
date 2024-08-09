458db4f02f7cf6c3577639cfae2fdfa216027c2f
tangqiuqiu <dawn.tang@temu.com>
2024-08-09 11:59:12 +0800

    feat: bugFix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # 把 public 文件复制到 dist 中
 cp -r public/* dist
-name="./dom_shot_ext_v0.1.1"
+name="./dom_shot_ext_v0.1.2"
 mkdir "$name"
 cp -r dist/* "./$name"
 zip -q -r "./dist/$name.zip" "./$name"
